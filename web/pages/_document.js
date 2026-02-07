efabe24a5b380e5457cdc5625cb63742e09f7287
Mihika Sarwate <mihika@example.com>
2026-02-07 20:02:14 +0530

    Fix: Resolve 404 favicon error on GitHub Pages by adding basePath prefix

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const isProd = process.env.NODE_ENV === 'production'
+  const basePath = isProd ? '/shalini' : ''
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
+      </Head>
       <body>
         <Main />
         <NextScript />
